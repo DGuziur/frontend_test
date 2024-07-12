70104772c2bc27d481fd323c1f1ae2fe652fab88
DGuziur <43893166+DGuziur@users.noreply.github.com>
2024-07-12 13:42:13 +0200

    create radio component

---
+++ task-app/src/app/shared/components/radio/radio.component.ts
@@ -0,0 +1,15 @@
+import { Component, input, model } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-radio',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './radio.component.html',
+  styleUrl: './radio.component.scss',
+})
+export class RadioComponent {
+  name = input.required<string>();
+  value = input.required<string>();
+  label = input.required<string>();
+}
