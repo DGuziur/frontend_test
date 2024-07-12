cd69b2bd25558645c254cc2f807c79814d4b9943
DGuziur <43893166+DGuziur@users.noreply.github.com>
2024-07-12 13:42:46 +0200

    create header component

---
+++ task-app/src/app/shared/components/header-component/header-component.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'header-component',
+  standalone: true,
+  imports: [],
+  templateUrl: './header-component.component.html',
+  styleUrl: './header-component.component.scss',
+})
+export class HeaderComponent {}
